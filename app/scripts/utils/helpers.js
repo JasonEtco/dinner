b53b00fb19542da8b068eeba9b846cef501ff08d
jasonetco <jasonetco@gmail.com>
2016-12-08 12:46:45 -0500

    :wrench: Fix SCSS selector bug

--- app/scripts/utils/helpers.js
+++ app/scripts/utils/helpers.js
@@ -13,12 +13,13 @@ const helpers = {
     //     'So how\'s school?',
     //   ],
     // },
-    // 'Crazy Uncle': {
-    //   chance: 3,
-    //   phrases: [
-    //     'Example',
-    //   ],
-    // },
+    'Crazy Uncle': {
+      chance: 3,
+      phrases: [
+        'Hey kid, you want some rum?',
+        'Let me tell you about your mom when she was a kid...',
+      ],
+    },
     // 'Art teacher Aunt': {
     //   chance: 3,
     //   phrases: [
@@ -26,27 +27,30 @@ const helpers = {
     //     'Make love not war man',
     //   ],
     // },
-    // 'Conservative Dad': {
-    //   chance: 3,
-    //   phrases: [
-    //     'Example',
-    //   ],
-    // },
-    // Baby: {
-    //   chance: 10,
-    //   phrases: [
-    //     'Goo goo! Gagagaga!',
-    //     'WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    //     '👶',
-    //     '🍼',
-    //     '👼',
-    //   ],
-    // },
+    'Conservative Dad': {
+      chance: 3,
+      phrases: [
+        'Now let\'s make Canada great again.',
+        'Immigrants, am I right?',
+        'You need to get a job. A real job.',
+      ],
+    },
+    Baby: {
+      chance: 10,
+      phrases: [
+        'Goo goo! Gagagaga!',
+        'WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        '👶',
+        '🍼',
+        '👼',
+      ],
+    },
     // Dog: {
     //   chance: 10,
     //   phrases: [
     //     'Arf! Arf! Food!',
-    //     'I just pooped in that lady\'s purse. SQUIRREL?',
+    //     'I just pooped in that lady\'s purse.',
+    //     'SQUIRREL?',
     //     '🐶',
     //     'BORK BORK BORK',
     //   ],
@@ -58,20 +62,20 @@ const helpers = {
         'Quoi?',
       ],
     },
-    'Random Family Friend': {
-      chance: 3,
-      phrases: [
-        'Example',
-      ],
-    },
-    // 'Bothersome Cousin': {
-    //   chance: 4,
+    // 'Random Family Friend': {
+    //   chance: 3,
     //   phrases: [
-    //     'Can we go play Wii now?',
-    //     'I\'m bored.',
-    //     'I\'M TELLING MY DAD YOU SAID THAT',
+    //     'How\'s school?',
     //   ],
     // },
+    'Bothersome Cousin': {
+      chance: 4,
+      phrases: [
+        'Can we go play Wii now?',
+        'I\'m bored.',
+        'I\'M TELLING MY DAD YOU SAID THAT',
+      ],
+    },
   },
   isEmoji(str) {
     // const ranges = [
